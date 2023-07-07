490120b4c4dc6243fe162801aae0dd633ed9e006
jack <252797991@qq.com>
2023-07-07 09:08:41 +0800

    first commit

---
+++ WRSHttpServer.h
@@ -0,0 +1,16 @@
+//
+//  WRSHttpServer.h
+//  WRSHttpServer
+//
+//  Created by Admin on 2023/7/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WRSHttpServer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WRSHttpServer.m
@@ -0,0 +1,12 @@
+//
+//  WRSHttpServer.m
+//  WRSHttpServer
+//
+//  Created by Admin on 2023/7/7.
+//
+
+#import "WRSHttpServer.h"
+
+@implementation WRSHttpServer
+
+@end
